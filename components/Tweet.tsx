4cd141ee9bb367973f1c5e96ec2531429ec56205
roykimm <roykimmyunghoon@gmail.com>
2022-05-18 12:57:37 +0900

    comment!

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tweet } from '../typings'
 import TimeAgo from "react-timeago";
 import {
@@ -7,11 +7,23 @@ import {
     SwitchHorizontalIcon,
     UploadIcon
 } from "@heroicons/react/outline"
+import { fetchComments } from '../utils/fetchComments';
 
 interface Props {
     tweet: Tweet
 }
+
 function Tweet({ tweet }: Props) {
+
+    const [ comments, setComments] = useState<Comment[]>([]);
+    const refreshComments = async () => {
+        const comments : Comment[] = await fetchComments(tweet._id);
+
+        setComments(comments);
+    }
+
+
+
     return (
         <div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
             <div className="flex space-x-3">
@@ -20,7 +32,7 @@ function Tweet({ tweet }: Props) {
                 <div>
                     <div className="flex items-center space-x-1">
                         <p className="mr-1 font-bold">{tweet.username}</p>
-                        <p className="hidden text-sm text-gray-500 sm:inline">@{tweet.username.replace(/\s+/g, '').toLowerCase()}</p>
+                        <p className="hidden text-sm text-gray-500 sm:inline">@{tweet.username.replace(/\s+/g, '').toLowerCase()} - </p>
                         <TimeAgo
                             className="text-sm text-gray-500"
                             date={tweet._createdAt}
@@ -33,18 +45,19 @@ function Tweet({ tweet }: Props) {
                 </div>
             </div>
 
-            <div>
-                <div>
-
+            <div className="mt-5 flex justify-between">
+                <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+                    <ChatAlt2Icon className="h-5 w-5" />
+                    <p>5</p>
                 </div>
-                <div>
-
+                <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+                    <SwitchHorizontalIcon className="h-5 w-5" />
                 </div>
-                <div>
-
+                <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+                    <HeartIcon className="h-5 w-5" />
                 </div>
-                <div>
-
+                <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+                    <UploadIcon className="h-5 w-5" />
                 </div>
 
             </div>
