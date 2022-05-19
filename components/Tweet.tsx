76476da69f40666cf13bccb93f8fd0661ab90a9c
roykimm <roykimmyunghoon@gmail.com>
2022-05-19 14:01:17 +0900

    t

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Tweet } from '../typings'
+import React, { useEffect, useState } from 'react'
+import { Comment, Tweet } from '../typings'
 import TimeAgo from "react-timeago";
 import {
     ChatAlt2Icon,
@@ -22,7 +22,11 @@ function Tweet({ tweet }: Props) {
         setComments(comments);
     }
 
+    useEffect(() => {
+        refreshComments();
+    },[])
 
+    console.log(comments);
 
     return (
         <div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
@@ -59,8 +63,39 @@ function Tweet({ tweet }: Props) {
                 <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
                     <UploadIcon className="h-5 w-5" />
                 </div>
-
             </div>
+
+            {/* Comments */}
+            {
+                comments?.length > 0 && (
+                    <div className="my-2 mt-5 max-h-44 space-x-5 overflow-y-scroll border-t border-gray-100 p-5">
+                        {
+                            comments.map(comment => (
+                                <div key={comment._id}  className="relative flex space-x-2">
+                                    <hr className="absolute left-5 top-10 h-0 border-x border-twitter/30" />
+                                    <img 
+                                        src={comment.profileImg} 
+                                        className="mt-2 h-7 w-7 rounded-full object-cover"
+                                        alt="" />
+                                    <div className="">
+                                        <div className="flex items-center space-x-1">
+                                          <p className="mr-1 font-bold">{comment.username}</p>  
+                                          <p className="hidden text-sm text-gray-500 lg:inline">
+                                              @{comment.username.replace(/\s+/g,'').toLowerCase()} -
+                                          </p> 
+                                          <TimeAgo
+                                            className="text-sm text-gray-500"
+                                            date={comment._createdAt}
+                                        />
+                                        </div>
+                                        <p>{comment.comment}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                )
+            }
         </div>
     )
 }
